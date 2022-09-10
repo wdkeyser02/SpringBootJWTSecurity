5d4bbb6716ac06d56c23ae37a2d65791d2d64bf9
willydekeyser <willy.de.keyser@skynet.be>
2022-09-10 12:16:36 +0200

    Setup Spring Boot JWT Security Samples part1

---
+++ SpringBootJWTSecuritySamplesPart1/src/main/java/willydekeyser/SpringBootJwtSecuritySamplesPart1Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtSecuritySamplesPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtSecuritySamplesPart1Application.class, args);
+	}
+
+}
