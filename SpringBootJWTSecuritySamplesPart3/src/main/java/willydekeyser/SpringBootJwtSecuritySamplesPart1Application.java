6dc2e4f2d17b828cb1864e664e1446d5e4d33f1b
willydekeyser <willy.de.keyser@skynet.be>
2022-09-12 14:30:35 +0200

    Ùpdate Config

--- SpringBootJWTSecuritySamplesPart3/src/main/java/willydekeyser/SpringBootJwtSecuritySamplesPart1Application.java
+++ SpringBootJWTSecuritySamplesPart3/src/main/java/willydekeyser/SpringBootJwtSecuritySamplesPart1Application.java
@@ -2,7 +2,11 @@ package willydekeyser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import willydekeyser.config.RsaKeyProperties;
+
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class SpringBootJwtSecuritySamplesPart1Application {
 
--- SpringBootJWTSecuritySamplesPart3/src/main/java/willydekeyser/config/SecurityConfig.java
+++ SpringBootJWTSecuritySamplesPart3/src/main/java/willydekeyser/config/SecurityConfig.java
@@ -8,12 +8,28 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+	private final RsaKeyProperties rsaKeys;
+	
 	@Bean
 	public InMemoryUserDetailsManager user() {
 		return new InMemoryUserDetailsManager(
@@ -34,5 +50,17 @@ public class SecurityConfig {
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.httpBasic(withDefaults())
 				.build();
+	}
+	
+	@Bean
+	JwtDecoder jwtDecoder() {
+		return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+	}
+	
+	@Bean
+	JwtEncoder jwtEncoder() {
+		JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+		JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+		return new NimbusJwtEncoder(jwks);
 	}
 }
